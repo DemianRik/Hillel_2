48f447c851516f67db054713740afe06e3d9831f
demianrikhard <demianrikhard@outlook.com>
2025-06-15 20:48:39 +0300

    "Commit_2"

--- HW_4.3.py
+++ HW_4.3.py
@@ -1,4 +1,5 @@
 import random
-list_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-list_random = random.sample(list_1, 3)
+list_1 = random.randint(3, 10)
+list_random = [random.randint(1, 100) for _ in range(list_1)]
+
 print(list_random)
