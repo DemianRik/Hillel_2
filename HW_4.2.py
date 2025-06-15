5d12ceb3bd8bb8de2b3d89e202552c858998780d
demianrikhard <demianrikhard@outlook.com>
2025-06-15 20:38:10 +0300

    "Commit_2"

--- HW_4.2.py
+++ HW_4.2.py
@@ -1,8 +1,13 @@
-list_1 = [0, 1, 7, 2, 4, 8]
-par_list = list_1[::2]
-sum_list = sum(par_list)
-element = list_1[-1]
+list_1 = []
+#list_1 = [0, 1, 7, 2, 4, 8]
 
-print("Сума парних елементів: ", sum_list)
-print("Множимо на: ", element)
-print("Результат: ", sum_list * element)
+if not list_1:
+    print("Список порожній.", 0)
+else:
+    par_list = list_1[::2]
+    sum_list = sum(par_list)
+    element = list_1[-1]
+
+    print("Сума парних елементів: ", sum_list)
+    print("Множимо на: ", element)
+    print("Результат: ", sum_list * element)
