58a18e05b58ba8f24098843cc9d68694b9553fd5
demianrikhard <demianrikhard@outlook.com>
2025-06-18 15:45:43 +0300

    "Smart_calculator"

---
+++ HW_5.2.py
@@ -0,0 +1,23 @@
+while True:
+    a = float(input("Перше число: "))
+    b = float(input("Друге число: "))
+    c = input("Що робимо? (+, -, *, /) ")
+
+    if c == '+':
+        print("Результат:", a + b)
+    elif c == '-':
+        print("Результат:", a - b)
+    elif c == '*':
+        print("Результат:", a * b)
+    elif c == '/':
+        if b == 0:
+            print("Помилка: ділення на нуль!")
+        else:
+            print("Результат:", a / b)
+    else:
+        print("Невідома операція!")
+
+    work_next = input("Бажаєте зробити ще одне обчислення? (y/yes щоб продовжити): ")#.lower()
+    if work_next not in ('y', 'yes'):
+        print("Дякую за користування калькулятором!")
+        break
