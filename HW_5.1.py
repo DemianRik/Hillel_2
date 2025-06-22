77980ff6530092a50836fc640864f5349dabfa62
demianrikhard <demianrikhard@outlook.com>
2025-06-22 15:20:49 +0300

    "name_var"

---
+++ HW_5.1.py
@@ -0,0 +1,13 @@
+import string
+import keyword
+name = input("Введіть можливе ім'я змінної: ")
+
+result = (
+    name.isidentifier() and
+    name not in keyword.kwlist and
+    not any(char in string.punctuation.replace("_", "") for char in name) and
+    name == name.lower() and
+    not name[0].isdigit() and
+    name.count("_") <= 1)
+
+print(result)
