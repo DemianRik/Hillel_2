c8db757faadd083553b848d1b46e8e27717a06fe
demianrikhard <demianrikhard@outlook.com>
2025-06-22 15:23:34 +0300

    "name_var"

--- HW_5.1.py
+++ HW_5.1.py
@@ -2,8 +2,7 @@ import string
 import keyword
 name = input("Введіть можливе ім'я змінної: ")
 
-result = (
-    name.isidentifier() and
+result = (name.isidentifier() and
     name not in keyword.kwlist and
     not any(char in string.punctuation.replace("_", "") for char in name) and
     name == name.lower() and
