70b14f2cc3cc344bb49dd37f8b5481a4f461a4ca
demianrikhard <demianrikhard@outlook.com>
2025-06-24 23:50:36 +0300

    "Latters_a-c_1"

--- HW_5.1.py
+++ HW_5.1.py
@@ -1,12 +1,13 @@
 import string
 import keyword
+
 name = input("Введіть можливе ім'я змінної: ")
 
 result = (name.isidentifier() and
-    name not in keyword.kwlist and
-    not any(char in string.punctuation.replace("_", "") for char in name) and
-    name == name.lower() and
-    not name[0].isdigit() and
-    not all(char == "_" for char in name))
+          name not in keyword.kwlist and
+          not any(char in string.punctuation.replace("_", "") for char in name) and
+          name == name.lower() and
+          not name[0].isdigit() and
+          not (all(char == "_" for char in name) and len(name) > 1))
 
 print(result)
